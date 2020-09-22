8345b8233d97f4f605c067f68e99acfc89deb439
unknown <abood25797@gmail.com>
2020-09-22 19:42:49 +0200

    first edit for landing page

--- js/app.js
+++ js/app.js
@@ -40,62 +40,88 @@ for (let i = 1; i <= 4; i++) {
   myCustomNav.appendChild(newList);
 }
 
+// $(function () {
+//   "use strict";
+
+//   $('.navbar-icon').click(function () {
+//       $('.navbar .collapse').slideToggle();
+//   });
+// });
+const myNav = document.getElementById("navbar-icon");
+
+myNav.addEventListener("click", function () {
+  const myCollapse = document.getElementById("collapse");
+  myCollapse.classList.toggle("slide");
+});
+
 // Add class 'active' to section when near top of viewport
 const setActive = document.getElementsByClassName("nav-item");
-  for(let i = 0; i < setActive.length; i++)
-  {
-    setActive[i].onclick = function(){
-          
-          // remove class from sibling
-          
-          let el = setActive[0];
-          while(el)
-          {
-              if(el.tagName === "LI"){
-                  //remove class
-                  el.classList.remove("active");
-                  
-              }
-              // pass to the new sibling
-              el = el.nextSibling;
-          }
-          
-        this.classList.add("active");  
-      };
-  }
+for (let i = 0; i < setActive.length; i++) {
+  setActive[i].onclick = function () {
+    // remove class from sibling
+
+    let el = setActive[0];
+    while (el) {
+      if (el.tagName === "LI") {
+        //remove class
+        el.classList.remove("active");
+      }
+      // pass to the new sibling
+      el = el.nextSibling;
+    }
+
+    this.classList.add("active");
+  };
+}
 
 // Scroll to anchor ID using scrollTO event
-let sectionOne    = document.querySelector('#section1'),
-    sectionTwo    = document.querySelector('#section2'),
-    sectionThree  = document.querySelector('#section3'),
-    sectionFour   = document.querySelector('#section4');
-  
-window.addEventListener('scroll', function() {
+let sectionOne = document.querySelector("#section1"),
+  sectionTwo = document.querySelector("#section2"),
+  sectionThree = document.querySelector("#section3"),
+  sectionFour = document.querySelector("#section4");
+
+window.addEventListener("scroll", function () {
   const windo = window.pageYOffset + 200,
-        listOne = document.querySelector('li:nth-child(1)'),
-        listTwo = document.querySelector('li:nth-child(2)'),
-        listThree = document.querySelector('li:nth-child(3)'),
-        listFour = document.querySelector('li:nth-child(4)');
-  if(sectionOne.offsetTop <= windo && sectionTwo.offsetTop > windo) {
+    listOne = document.querySelector("li:nth-child(1)"),
+    listTwo = document.querySelector("li:nth-child(2)"),
+    listThree = document.querySelector("li:nth-child(3)"),
+    listFour = document.querySelector("li:nth-child(4)");
+  if (sectionOne.offsetTop <= windo && sectionTwo.offsetTop > windo) {
     listOne.setAttribute("class", "active");
     listTwo.removeAttribute("class", "active");
     listThree.removeAttribute("class", "active");
     listFour.removeAttribute("class", "active");
-  } else if(sectionTwo.offsetTop <= windo && sectionThree.offsetTop > windo) {
+    sectionOne.setAttribute("class", "your-active-class");
+    sectionTwo.removeAttribute("class", "your-active-class");
+    sectionThree.removeAttribute("class", "your-active-class");
+    sectionFour.removeAttribute("class", "your-active-class");
+  } else if (sectionTwo.offsetTop <= windo && sectionThree.offsetTop > windo) {
     listTwo.setAttribute("class", "active");
     listOne.removeAttribute("class", "active");
     listThree.removeAttribute("class", "active");
     listFour.removeAttribute("class", "active");
-  } else if(sectionThree.offsetTop <= windo && sectionFour.offsetTop > windo) {
+    sectionTwo.setAttribute("class", "your-active-class");
+    sectionOne.removeAttribute("class", "your-active-class");
+    sectionThree.removeAttribute("class", "your-active-class");
+    sectionFour.removeAttribute("class", "your-active-class");
+  } else if (sectionThree.offsetTop <= windo && sectionFour.offsetTop > windo) {
     listThree.setAttribute("class", "active");
     listTwo.removeAttribute("class", "active");
     listOne.removeAttribute("class", "active");
     listFour.removeAttribute("class", "active");
-  } else if(sectionFour.offsetTop <= windo) {
+    sectionThree.setAttribute("class", "your-active-class");
+    sectionTwo.removeAttribute("class", "your-active-class");
+    sectionOne.removeAttribute("class", "your-active-class");
+    sectionFour.removeAttribute("class", "your-active-class");
+  } else if (sectionFour.offsetTop <= windo) {
     listFour.setAttribute("class", "active");
     listTwo.removeAttribute("class", "active");
     listThree.removeAttribute("class", "active");
     listOne.removeAttribute("class", "active");
+    sectionFour.setAttribute("class", "your-active-class");
+    sectionTwo.removeAttribute("class", "your-active-class");
+    sectionThree.removeAttribute("class", "your-active-class");
+    sectionOne.removeAttribute("class", "your-active-class");
   }
 });
 
